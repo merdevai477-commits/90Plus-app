f0aea05b0e3a0a5882fabc4319002a2733cd5104
Mr.dev__ <111096416+scoobar1@users.noreply.github.com>
2026-05-16 22:03:06 +0300

    chore: remove AUTH reference folder (already integrated)

--- AUTH/index.ts
+++
@@ -1,4 +0,0 @@
-export { AuthScreenShell } from './AuthScreenShell';
-export { AuthTextField } from './AuthTextField';
-export { AuthHeroBlock } from './AuthHeroBlock';
-export { AUTH_ACCENT, AUTH_PANEL_BG } from './AuthTokens';
